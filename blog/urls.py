aa445f9b404bd24631dd5baa6507cdb4be05148e
yeessonng <dorlek856@gmail.com>
2025-05-30 17:34:02 +0900

    feat: fbv로 블로그 상세 페이지

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views #현재 폴더 - bolg에 있는 views.py를 사용할 수 있게 가져와라
 
 urlpatterns = [
+    path('<int:pk>/', views.single_post_page),#정수 형태의 pk 값이 붙는 url이라면 해당 함수에 정의된 대로 처리
     path('', views.index), #view의 index함수를 실행
 ]
--- blog/views.py
+++ blog/views.py
@@ -12,3 +12,14 @@ def index(request):
             'posts': posts,
         }
     )
+
+def single_post_page(request, pk):
+    post = Post.objects.get(pk=pk) #괄호 안에 조건을 만족하는 post 레코드를 가져와라
+
+    return render(
+        request,
+        'blog/single_post_page.html',
+        {
+            'post': post, #가져온 레코드 하나를 html 페이지에 담아 렌더링
+        }
+    )
