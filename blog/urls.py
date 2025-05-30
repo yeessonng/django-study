3248705105722fa1d1560e767ff2b40755783c49
yeessonng <dorlek856@gmail.com>
2025-05-31 00:55:52 +0900

    feat: cbv로 상세 페이지 수정

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views #현재 폴더 - bolg에 있는 views.py를 사용할 수 있게 가져와라
 
 urlpatterns = [
-    path('<int:pk>/', views.single_post_page),#정수 형태의 pk 값이 붙는 url이라면 해당 함수에 정의된 대로 처리
+    path('<int:pk>/', views.PostDetail.as_view()),
     path('', views.PostList.as_view())
+    # path('<int:pk>/', views.single_post_page),  # 정수 형태의 pk 값이 붙는 url이라면 해당 함수에 정의된 대로 처리
     # path('', views.index), #view의 index함수를 실행
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 # Create your views here.
 class PostList(ListView):
     model = Post #여러 post를 나열할 때 - CBV ListView를 사용할 것, model은 post다
     ordering = '-pk'
+
+class PostDetail(DetailView):
+    model = Post
 
 # def index(request):
 #     posts = Post.objects.all().order_by('-pk') #모든 post 레코드를 가져와 posts에 저장 - all() 다 가져와라, order_by('-pk') 내림차순으로 가져와라
@@ -18,13 +20,13 @@ class PostList(ListView):
 #         }
 #     )
 
-def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk) #괄호 안에 조건을 만족하는 post 레코드를 가져와라
-
-    return render(
-        request,
-        'blog/single_post_page.html',
-        {
-            'post': post, #가져온 레코드 하나를 html 페이지에 담아 렌더링
-        }
-    )
+# def single_post_page(request, pk):
+#     post = Post.objects.get(pk=pk) #괄호 안에 조건을 만족하는 post 레코드를 가져와라
+#
+#     return render(
+#         request,
+#         'blog/post_detail.html',
+#         {
+#             'post': post, #가져온 레코드 하나를 html 페이지에 담아 렌더링
+#         }
+#     )
