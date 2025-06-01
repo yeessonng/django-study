1ba1495c0bcfa6842b7981590544b85cb84aa469
yeessonng <dorlek856@gmail.com>
2025-06-01 23:33:30 +0900

    feat: 미리보기, ã요약문 추가

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,7 @@ import os
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=30)
+    hook_text = models.CharField(max_length=100, blank=True)
     content = models.TextField() #문자 길이 제한 x
 
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True) #이미지를 저장할 폴더의 경로 규칙 지정
