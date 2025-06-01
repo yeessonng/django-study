da1695688c3a3b93781cd7dce225a50c7b711ecf
yeessonng <dorlek856@gmail.com>
2025-06-01 22:19:04 +0900

    feat: post list, detail page에서 head image 없을 때 문제 해결

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,7 @@ class Post(models.Model):
     content = models.TextField() #문자 길이 제한 x
 
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True) #이미지를 저장할 폴더의 경로 규칙 지정
+    file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
