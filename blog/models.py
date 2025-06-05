9b6d6264687151667e38bd709750eea06acaaa8d
yeessonng <dorlek856@gmail.com>
2025-06-05 18:44:23 +0900

    feat: author 필드 추가

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User #장고에서 기본적으로 제공하는 모델
 import os
 
 # Create your models here.
@@ -9,11 +10,14 @@ class Post(models.Model):
 
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True) #이미지를 저장할 폴더의 경로 규칙 지정
     file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
+    
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    
+    author = models.ForeignKey(User, on_delete=models.CASCADE) #User를 사용해 author 필드 구성 / 이 post 작성자가 db에서 삭제되었을 때 이 포스트도 같이 삭제한다.
 
     def __str__(self):
-        return f'[{self.pk}]{self.title}'
+        return f'[{self.pk}]{self.title} :: {self.author}'
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/' #문자열 포맷팅f
@@ -23,3 +27,4 @@ class Post(models.Model):
 
     def get_file_ext(self): #확장자 찾기
         return self.get_file_name().split('.')[-1] #-1은 마지막 요소 ex) jpg
+
